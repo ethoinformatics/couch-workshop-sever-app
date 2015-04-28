cf1e3b623f84bcc4009ba413f0ccfffb521f615a
Mike Chevett <mchevett@gmail.com>
2015-04-28 08:40:30 -0400

    add a dist dir

--- index.js
+++ index.js
@@ -2,22 +2,31 @@ require('./index.less');
 
 var dataTmpl = require('./data.vash');
 var Pouch = require('pouchdb');
-var db = new Pouch('some_db');
+var db = new Pouch('some_db2');
 var $ = require('jquery');
 
-$(function(){
-	db.replicate.from($('#url').val(), {live: false})
-		.on('complete', function(info){
-			db.allDocs({include_docs: true, descending: true})
-				.then(function(res){
-					var docs = res.rows.map(function(item){return item.doc;});
-					$('body').append(dataTmpl({docs: docs}));
-				});
-		})
-		.on('error', function(err){
-			console.error('Download errror: ' + err.message);
-			console.error(err);
-		});
+function replicateFrom(){
+db.replicate.from($('#url').val(), {live: false})
+	.on('complete', function(info){
+		db.allDocs({include_docs: true, descending: true})
+			.then(function(res){
+				var docs = res.rows.map(function(item){return item.doc;});
+				var $div = $('<div class="results"></div');
+				$div.append(dataTmpl({docs: docs}));
+				$('body').append($div);
+			});
+	})
+	.on('error', function(err){
+		console.error('Download errror: ' + err.message);
+		console.error(err);
+	});
+}
 
+$(function(){
+
+	$('button').click(function(){
+		$('.results').fadeOut('fast');
+		replicateFrom();
+	});
 });
 
