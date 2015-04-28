d51d1539a662be804078f986caf17b18e3c9e078
Mike Chevett <mchevett@gmail.com>
2015-04-28 08:48:43 -0400

    always show the docs from the local database

--- index.js
+++ index.js
@@ -5,24 +5,29 @@ var Pouch = require('pouchdb');
 var db = new Pouch('some_db2');
 var $ = require('jquery');
 
+function renderDocs(){
+	db.allDocs({include_docs: true, descending: true})
+		.then(function(res){
+			var docs = res.rows.map(function(item){return item.doc;});
+			var $div = $('<div class="results"></div');
+			$div.append(dataTmpl({docs: docs}));
+			$('body').append($div);
+		});
+}
 function replicateFrom(){
-db.replicate.from($('#url').val(), {live: false})
-	.on('complete', function(info){
-		db.allDocs({include_docs: true, descending: true})
-			.then(function(res){
-				var docs = res.rows.map(function(item){return item.doc;});
-				var $div = $('<div class="results"></div');
-				$div.append(dataTmpl({docs: docs}));
-				$('body').append($div);
-			});
-	})
-	.on('error', function(err){
-		console.error('Download errror: ' + err.message);
-		console.error(err);
-	});
+	db.replicate.from($('#url').val(), {live: false})
+		.on('complete', function(info){
+			renderDocs();
+		})
+		.on('error', function(err){
+			window.alert('Download errror: ' + err.message);
+			console.error(err);
+		});
 }
 
 $(function(){
+
+	renderDocs();
 
 	$('button').click(function(){
 		$('.results').fadeOut('fast');
